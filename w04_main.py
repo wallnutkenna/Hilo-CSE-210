571ce4ae51c086e64889c65b45bca687400a8f52
Ale Bravo <bra21043@byu.edu>
2022-01-26 21:34:46 -0600

    Second attempt for game loop from main
    This is the second attempt for the game loop from the main function

--- w04_main.py
+++ w04_main.py
@@ -1,20 +1,19 @@
+keep_playing = True
 
 def main ():
     
     score = 300
     deck = 13
-
-    # print (f"The card is: 9")
-    # print ("Higher or lower? [h/l] L")
-    # print (f"Next card was: 5")
     score += 100
 
     if score > 0 and deck != 0:
         play_again = input("Play again? [y/n] ")
         print ()
 
-    if play_again == "y":
-        keep_playing = True
+    if play_again == "n":
+        global keep_playing 
+        keep_playing = False
+        print ("\nThank you for playing")
 
 while keep_playing:
     if __name__ == "__main__":
