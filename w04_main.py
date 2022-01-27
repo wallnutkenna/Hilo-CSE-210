ec20f961e925ca10a9476e0d2d209dcde02c5bd7
Ale Bravo <bra21043@byu.edu>
2022-01-26 21:56:53 -0600

    Third attempt for the game loop in w04_main
    This is the third attempt for the game loop from the main function

--- w04_main.py
+++ w04_main.py
@@ -1,19 +1,22 @@
 keep_playing = True
 
 def main ():
-    
     score = 300
     deck = 13
-    score += 100
+    play_again= ""
 
-    if score > 0 and deck != 0:
+    if score > 0 and deck > 0:
         play_again = input("Play again? [y/n] ")
         print ()
 
-    if play_again == "n":
-        global keep_playing 
+        if play_again == "n":
+            global keep_playing 
+            keep_playing = False
+            print ("\nThank you for playing.")
+
+    else:
+        print ("Game Over.")
         keep_playing = False
-        print ("\nThank you for playing")
 
 while keep_playing:
     if __name__ == "__main__":
